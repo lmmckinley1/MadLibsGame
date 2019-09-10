cce548a0a59620ffb4fd27a6f3e257582eec3aca
lmmckinley1 <lmmckinley1@ecc-jaguar.csuchico.edu>
2019-09-09 17:22:29 -0700

    Madlib created

--- madlibs.cpp
+++ madlibs.cpp
@@ -1,9 +1,43 @@
-//Authors: 
+//Leah McKinley 
 #include <iostream>
 #include <string>
 
+using namespace std;
+using std::string;
+using std::cin;
+using std::cout;
+using std::endl;
+
 int main()
 {
+   
+string noun;
+cin>>noun;
+string place;
+cin>>place;
+string adjective;
+cin>>adjective;
+int  number = 12;
+string verb;
+cin>>verb;
 
-  return 0;
+cout<<"Enter a place:"<<endl;
+    cin>>place;
+    cout<<"Enter a noun:"<<endl;
+    cin>>noun;
+    cout<<"Enter a noun:"<<endl;
+    cin>>noun;
+    cout<<"Enter an adjective:"<<endl;
+    cin>>adjective;
+    cout<<"Enter a number:" <<endl;
+    cin>>number;
+    cout<<"Enter a verb:"<<endl;
+    cin>>verb;
+    cout<<"In" <<place;
+    cout<<"there was a" <<noun;
+    cout<<"looking for food. It stumbled upon a" <<noun;
+    cout<<"and to its surprise it was" <<noun;
+    cout<<".Then" <<number;
+    cout<< "monkeys jumped at them and they" <<verb;    
+    return 0;
 }
